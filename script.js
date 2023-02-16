5b807826f2a33dd855d5c67e43808c512accbefc
Juvi09 <ntuthuko.s.ndlovu@gmail.com>
2023-02-16 16:40:19 +0200

    any key function to increase count

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const increaseButtonEl = document.querySelector('.counter__button--increase');
 const decreaseButtonEl = document.querySelector('.counter__button--decrease');
+const resetButtonEl = document.querySelector('.counter__reset-button')
 const counterValueEl = document.querySelector('.counter__value');
 
+
+
+resetButtonEl.addEventListener('click', function(){
+    // set counter to 0
+    counterValueEl.textContent = 0;
+});
 
 
 decreaseButtonEl.addEventListener('click', function(){
@@ -27,4 +34,18 @@ increaseButtonEl.addEventListener('click', function(){
 
     // set counter element with new value
     counterValueEl.textContent = newValue;
+});
+
+document.addEventListener('keydown', function(){
+     // get current value of counter
+     const currentValue = counterValueEl.textContent;
+
+     // convert value to number type
+     const currentValueAsNumber = +currentValue;
+ 
+     // increment by 1
+     const newValue = currentValueAsNumber + 1;
+ 
+     // set counter element with new value
+     counterValueEl.textContent = newValue;
 });
